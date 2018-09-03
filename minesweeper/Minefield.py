9b35d32bcab278c1f5e86f01c92a478b3a1668b7
Gage Phillips <gagerdude@gmail.com>
2018-09-03 12:50:17 -0500

    Setup skeleton of Minefield.py

---
+++ minesweeper/Minefield.py
@@ -0,0 +1,20 @@
+from Space import Space
+
+class Minefield:
+
+    x_size = 0
+    y_size = 0
+    numMines = 0
+    minefield = []
+
+    def __init__(self, x_size, y_size):
+
+    def checkFlags():
+
+    def placeFlag(x, y):
+
+    def reveal(x, y):
+
+    def removeFlag(x, y):
+
+    def getSpace(x, y):
