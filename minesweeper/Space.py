9de49178e9729bb7c2c33b8f92a5e00ee9dee79f
Thomas Gartman <thomasgartman@ku.edu>
2018-09-03 15:28:16 -0500

    Guarding Space.py

--- minesweeper/Space.py
+++ minesweeper/Space.py
@@ -1,11 +1,12 @@
-class Space:
-    def __init__(self, x_loc, y_loc):
-        self.x_loc = x_loc
-        self.y_loc = y_loc
-        self.isMine = False
-        self.isFlagged = False
-        self.isRevealed = False
-        self.numOfSurroundingMines = 0
+if __name__ == '__main__':
+    class Space:
+        def __init__(self, x_loc, y_loc):
+            self.x_loc = x_loc
+            self.y_loc = y_loc
+            self.isMine = False
+            self.isFlagged = False
+            self.isRevealed = False
+            self.numOfSurroundingMines = 0
 
 
 
