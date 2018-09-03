57110adb1d996b90e1942175562d40ff76107477
Zachary Atkins <zacharyjayhawk@gmail.com>
2018-09-03 14:14:22 -0500

    Created App class and refactored code

--- minesweeper/app.py
+++ minesweeper/app.py
@@ -1,39 +1,76 @@
 #!/usr/local/bin/python3
 import pygame
 from pygame.locals import *
+from Minefield import *
 
-def main():
-    BLACK = 0, 0, 0
-    WHITE = 255, 255, 255
+class App:
+	def __init__(self):
+		x_dim = 5
+		y_dim = 5
+		n_mines = 10
 
-    n = 5
-    size = (32*n + 4, 32*n + 4)
+		self.SPACE_WIDTH = 32
+		self.SPACE_HEIGHT = 32
+		self.WIDTH = self.SPACE_WIDTH*x_dim
+		self.HEIGHT = self.SPACE_HEIGHT*y_dim
 
-    spaceHeight = 32
-    spaceWidth = 32
+		self.minefield = Minefield(x_dim, y_dim, n_mines)
+		self.grid = self.minefield.minefield
 
-    pygame.init()
-    screen = pygame.display.set_mode(size)
-    pygame.display.set_caption("BitSweeper")
-    clock = pygame.time.Clock()
+		pygame.init()
+		self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+		self.screen.fill(Color('black'))
 
-    screen.fill(BLACK)
+		pygame.display.set_caption("BitSweeper")
+		clock = pygame.time.Clock()
+		
+	def onClick(self, event):
+		pos = event.pos()
+		realPos = (pos[0]/self.WIDTH, pos[1]/self.HEIGHT)
+		try:
+			activeSpace = self.minefield.getSpace(realPos)
+		except:
+			pass
+		if activeSpace.isRevealed:
+			pass
+		elif event.button == 1:
+			self.minefield.reveal(realPos)
+		elif event.button == 3:
+			if not activeSpace.isFlagged():
+				self.minefield.placeFlag
 
-    done = False
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-        
-        screen.fill(BLACK)
+def main():	
+	app = App()
 
-        pygame.display.flip()
+	
 
-        clock.tick(60)
-    
-    pygame.quit()
+	screen.fill(BLACK)
+
+	done = False
+	while not done:
+		for event in pygame.event.get():
+			# Quit Event 
+			if event.type == pygame.QUIT:
+				done = True
+
+			
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				pos = event.pos()
+				realPos = (pos[0]/WIDTH, pos[1]/HEIGHT)
+				if event.button == 1:
+					minefield.reveal(realPos)
+				elif event.button == 3:
+					minefield.placeFlag
+
+		screen.fill(BLACK)
+
+		pygame.display.flip()
+
+		clock.tick(60)
+	
+	pygame.quit()
 
 if __name__ == '__main__': main()
 
-    
+		
 
