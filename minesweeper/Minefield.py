3658bb277046083d6b685fe6587921075a242f56
Gage Phillips <gagerdude@gmail.com>
2018-09-03 15:35:27 -0500

    removed if's added for pydoc. was causing errors. for later/different branch?

--- minesweeper/Minefield.py
+++ minesweeper/Minefield.py
@@ -1,6 +1,5 @@
-if __name__ == "__main__":
-    from Space import Space
-    import random
+from Space import Space
+import random
 
 class Minefield:
 
--- minesweeper/Space.py
+++ minesweeper/Space.py
@@ -1,13 +1,12 @@
-if __name__ == '__main__':
-    class Space:
-        def __init__(self, x_loc, y_loc):
-            self.x_loc = x_loc
-            self.y_loc = y_loc
-            self.isMine = False
-            self.isFlagged = False
-            self.isRevealed = False
-            self.numOfSurroundingMines = 0
+class Space:
+	def __init__(self, x_loc, y_loc):
+		self.x_loc = x_loc
+		self.y_loc = y_loc
+		self.isMine = False
+		self.isFlagged = False
+		self.isRevealed = False
+		self.numOfSurroundingMines = 0
 
 
 
-    
+
