a8369fcc49116dcf747c4eeb67ce0909336fe638
Zachary Atkins <zacharyjayhawk@gmail.com>
2018-09-07 13:12:47 -0500

    add gameScreen property to Window

--- minesweeper/Window.py
+++ minesweeper/Window.py
@@ -4,16 +4,25 @@ from pygame.locals import *
 class Window:
 	def __init__(self, x_dim, y_dim):
 		self.SPACE_PIXELS = 32
-		self.MARGIN = self.SPACE_PIXELS/4
+		self.MARGIN = self.SPACE_PIXELS / 4
+		self.HEADER_BAR = self.SPACE_PIXELS * 3
 		self.WIDTH = self.SPACE_PIXELS*x_dim + 2 * self.MARGIN
-		self.HEIGHT = self.SPACE_PIXELS*y_dim + 2 * self.MARGIN
+		self.HEIGHT = self.SPACE_PIXELS*y_dim + 2 * self.MARGIN + self.HEADER_BAR
 
 		pygame.init()
 		pygame.font.init()
-		self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
-		self.screen.fill(Color('light grey'))
+		self._screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+		self._gameScreen = self._screen.subsurface(Rect(self.MARGIN, self.HEADER_BAR + self.MARGIN, self.SPACE_PIXELS*x_dim, self.SPACE_PIXELS*y_dim))
+		self._screen.fill(Color('light grey'))
+		self._gameScreen.fill(Color('black'))
 
-		
+	@property
+	def gameScreen(self):
+		return self._gameScreen
 
+	@gameScreen.setter
+	def setGameScreen(self, appSurface):
+		""" Adds appSurface as a subsurface to screen """
+		self._screen.subsurface(appSurface)
 
 
