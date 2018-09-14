8242fc50212497146c0a123982c0dfe918b52915
Gage Phillips <gagerdude@gmail.com>
2018-09-14 17:02:49 -0500

    Added text to the end screen, and a background to make that text readable

--- Minesweeper/EndScreen.py
+++ Minesweeper/EndScreen.py
@@ -2,24 +2,53 @@ import pygame
 from Minesweeper.StartScreen import StartScreen
 
 class EndScreen:
-    def __init__(self, gameWon, windowSize):
-        self.surf = pygame.Surface(windowSize, pygame.SRCALPHA)
-        self.destSurf = pygame.display.get_surface().copy()
-        self.drawWindow = pygame.display.get_surface()
-        self.gameWon = gameWon
-        self.winColor = (0,255, 0, 127)
-        self.loseColor = (255, 0, 0, 127)
+	def __init__(self, gameWon, windowSize):
+		self.surf = pygame.Surface(windowSize, pygame.SRCALPHA)
+		self.destSurf = pygame.display.get_surface().copy()
+		self.drawWindow = pygame.display.get_surface()
+		self.gameWon = gameWon
+		self.winColor = (0,255, 0, 127)
+		self.winTextBackgroundColor = (0,255,0)
+		self.loseColor = (255, 0, 0, 127)
+		self.loseTextBackgroundColor = (255,0,0)
+		self.title = pygame.font.SysFont('lucidaconsole', 25)
+		self.subtitle = pygame.font.SysFont('lucidaconsole', 15)
 
-        if self.gameWon:
-            self.surf.fill(self.winColor)
-        else:
-            self.surf.fill(self.loseColor)
-            
-    def render(self):
-        self.drawWindow.blits([
-            (self.destSurf, (0,0)),
-            (self.surf, (0,0))
-        ])
-        pygame.display.flip()
+		#distance between title and subtitle text
+		textMargin = 10
+		#distance between edge of text and full opacity background
+		textBackgroundMargin = 10
 
-        
+		if self.gameWon:
+			self.surf.fill(self.winColor)
+			self.titleSurf = self.title.render("You Won!", True, pygame.Color('black'))
+		else:
+			self.surf.fill(self.loseColor)
+			self.titleSurf = self.title.render("You Lost!", True, pygame.Color('black'))
+		self.subtitleSurf = self.subtitle.render("Click to play again", True, pygame.Color('black'))
+
+		self.textBackgroundSurf = pygame.Surface(((self.subtitleSurf.get_width() + 2*textBackgroundMargin),(self.titleSurf.get_height() + self.subtitleSurf.get_height() + textMargin + 2*textBackgroundMargin)))
+
+		if self.gameWon:
+			self.textBackgroundSurf.fill(self.winTextBackgroundColor)
+		else:
+			self.textBackgroundSurf.fill(self.loseTextBackgroundColor)
+
+		self.titlePos = (windowSize[0] / 2 - self.titleSurf.get_width() / 2, windowSize[1] / 2 - self.titleSurf.get_height() - textMargin / 2)
+		self.subtitlePos = (windowSize[0] / 2 - self.subtitleSurf.get_width() / 2, windowSize[1] / 2 + textMargin / 2)
+		self.textBackgroundPos = (windowSize[0] / 2 - self.textBackgroundSurf.get_width() / 2, windowSize[1] / 2 - self.textBackgroundSurf.get_height() / 2)
+	
+		
+		
+		
+
+	def render(self):
+		self.drawWindow.blit(self.destSurf, (0,0))
+		self.drawWindow.blit(self.surf, (0,0), None, pygame.BLEND_RGBA_MULT)
+		self.drawWindow.blits([
+			(self.textBackgroundSurf, self.textBackgroundPos),
+			(self.titleSurf, self.titlePos),
+			(self.subtitleSurf, self.subtitlePos)
+		])
+
+		pygame.display.flip()	
