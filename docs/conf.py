a00f2dcf0607e36dd13bffaa6070fcbb60e28fb8
Thomas Gartman <thomasgartman@ku.edu>
2018-09-16 13:35:40 -0500

    Fixed conf.py to make documentation only in Minesweeper

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('.'))
-sys.path.append('../')
+sys.path.append('../Minesweeper')
 
 # -- General configuration ------------------------------------------------
 
@@ -166,6 +166,3 @@ texinfo_documents = [
      author, 'Minesweeper', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
