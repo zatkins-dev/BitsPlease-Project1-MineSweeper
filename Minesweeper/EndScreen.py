da30a730a70446f5fa433105f8e0145a6aa37a23
Zachary Atkins <zacharyjayhawk@gmail.com>
2018-09-16 15:35:00 -0500

    cleaned up endscreen

--- Minesweeper/EndScreen.py
+++ Minesweeper/EndScreen.py
@@ -7,37 +7,42 @@ class EndScreen:
 		self.destSurf = display.get_surface().copy()
 		self.drawWindow = display.get_surface()
 		self.surf = surface.Surface(display.get_surface().get_size(), constants.SRCALPHA)
-		self.gameWon = gameWon
-		self.winColor = (0,255, 0, 127)
-		self.winTextBackgroundColor = (0,255,0)
-		self.loseColor = (255, 0, 0, 127)
-		self.loseTextBackgroundColor = (255,0,0)
-		self.title = SysFont('lucidaconsole', 25)
-		self.subtitle = SysFont('lucidaconsole', 15)
+
+		color = (0,255, 0, 127) if gameWon else (255, 0, 0, 127)
+		text = "You Won!" if gameWon else "You Lost..."
+		backgroundColor = (0,255,0) if gameWon else (255,0,0)
+		title = SysFont('lucidaconsole', 25)
+		subtitle = SysFont('lucidaconsole', 15)
 
 		#distance between title and subtitle text
 		textMargin = 10
 		#distance between edge of text and full opacity background
 		textBackgroundMargin = 10
 
-		if self.gameWon:
-			self.surf.fill(self.winColor)
-			self.titleSurf = self.title.render("You Won!", True, Color('black'))
-		else:
-			self.surf.fill(self.loseColor)
-			self.titleSurf = self.title.render("You Lost...", True, Color('black'))
-		self.subtitleSurf = self.subtitle.render("Click to play again", True, Color('black'))
+		self.surf.fill(color)
+		self.titleSurf = title.render(text, True, Color('black'))
+		self.subtitleSurf = subtitle.render("Click to play again", True, Color('black'))
 
-		self.textBackgroundSurf = surface.Surface(((self.subtitleSurf.get_width() + 2*textBackgroundMargin),(self.titleSurf.get_height() + self.subtitleSurf.get_height() + textMargin + 2*textBackgroundMargin)))
+		self.textBackgroundSurf = surface.Surface(
+			(self.subtitleSurf.get_width() + 2*textBackgroundMargin),
+			(self.titleSurf.get_height() + self.subtitleSurf.get_height() + textMargin + 2*textBackgroundMargin)
+		)
 
-		if self.gameWon:
-			self.textBackgroundSurf.fill(self.winTextBackgroundColor)
-		else:
-			self.textBackgroundSurf.fill(self.loseTextBackgroundColor)
 
-		self.titlePos = (self.drawWindow.get_width() / 2 - self.titleSurf.get_width() / 2, self.drawWindow.get_height() / 2 - self.titleSurf.get_height() - textMargin / 2)
-		self.subtitlePos = (self.drawWindow.get_width() / 2 - self.subtitleSurf.get_width() / 2, self.drawWindow.get_height() / 2 + textMargin / 2)
-		self.textBackgroundPos = (self.drawWindow.get_width() / 2 - self.textBackgroundSurf.get_width() / 2, self.drawWindow.get_height() / 2 - self.textBackgroundSurf.get_height() / 2)
+		self.textBackgroundSurf.fill(backgroundColor)
+
+		self.titlePos = (
+			self.drawWindow.get_width() / 2 - self.titleSurf.get_width() / 2,
+			self.drawWindow.get_height() / 2 - self.titleSurf.get_height() - textMargin / 2
+		)
+		self.subtitlePos = (
+			self.drawWindow.get_width() / 2 - self.subtitleSurf.get_width() / 2, 
+			self.drawWindow.get_height() / 2 + textMargin / 2
+		)
+		self.textBackgroundPos = (
+			self.drawWindow.get_width() / 2 - self.textBackgroundSurf.get_width() / 2,
+			self.drawWindow.get_height() / 2 - self.textBackgroundSurf.get_height() / 2
+		)
 
 	def render(self):
 		self.drawWindow.blit(self.destSurf, (0,0))
