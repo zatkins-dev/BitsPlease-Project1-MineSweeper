62009fb433717e1927d037af46ca3e115fe3b510
Zachary Atkins <zacharyjayhawk@gmail.com>
2018-09-16 15:40:33 -0500

    fixed indentation

--- Minesweeper/Space.py
+++ Minesweeper/Space.py
@@ -6,13 +6,13 @@ class Space:
 
 		*y_loc* = Integer location on the board. 0 is the top of the board.
 		
-                *isMine* = Boolean that logs if the space has a mine. True if it does, false otherwise.
-		
-                *isFlagged* = Boolean that logs if the user placed a flag on this space. True if the user did.
-		
-                *IsRevealed* = Boolean that logs if the contents of the space are revealed to the user. True if the user should see the contents.
-		
-                *numOfSurroundingMines* = Integer that tracks how many spaces in the 8 closest spaces have mines."""
+		*isMine* = Boolean that logs if the space has a mine. True if it does, false otherwise.
+
+		*isFlagged* = Boolean that logs if the user placed a flag on this space. True if the user did.
+
+		*IsRevealed* = Boolean that logs if the contents of the space are revealed to the user. True if the user should see the contents.
+
+		*numOfSurroundingMines* = Integer that tracks how many spaces in the 8 closest spaces have mines."""
 	def __init__(self, x_loc, y_loc):
 		self.x_loc = x_loc
 		self.y_loc = y_loc
