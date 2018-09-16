a56dfcffe4b899c9bda975637177ea5101af994e
Zachary Atkins <zacharyjayhawk@gmail.com>
2018-09-16 16:15:13 -0500

    Changed Drawer to use tuples

--- Minesweeper/Graphics/Drawer.py
+++ Minesweeper/Graphics/Drawer.py
@@ -4,19 +4,19 @@ class Drawer:
     def __init__(self):
         self.buttonIsClicked = False
         
-    def drawButton(self, destSurf, x, y, width, height, color, colorHover, buttonText, buttonTextSize, buttonFunction=None):
+    def drawButton(self, destSurf, pos, size, colors, buttonText, buttonTextSize, buttonFunction=None):
         mousePos = mouse.get_pos()
         t_font = font.SysFont('lucidaconsole', buttonTextSize)
         text = t_font.render(str(buttonText), True, (0,0,0))
 
-        button = surface.Surface((width, height), constants.SRCALPHA)
+        button = surface.Surface(size, constants.SRCALPHA)
 
         offset = destSurf.get_abs_offset()
 
         #see if mouse is within the area of our button
-        if mousePos[0] > x + offset[0] and mousePos[0] < x + width + offset[0] and mousePos[1] > y + offset[1] and mousePos[1] < y + height + offset[1]:
+        if mousePos[0] > pos[0] + offset[0] and mousePos[0] < pos[0] + size[0] + offset[0] and mousePos[1] > pos[1] + offset[1] and mousePos[1] < pos[1] + size[1] + offset[1]:
             #mouse is over the button
-            button.fill(colorHover)
+            button.fill(colors[1])
 			#mouse is in the button, so it may click the button and run its function
             if mouse.get_pressed()[0] and buttonFunction != None and not self.buttonIsClicked:
                 buttonFunction()
@@ -25,10 +25,10 @@ class Drawer:
                 self.buttonIsClicked = False
         else:
 			#mouse isn't in the button
-            button.fill(color)
+            button.fill(colors[0])
 
 		#put button onto the screen, then text onto the screen centered over the button
         destSurf.blits([
-            (button, (x, y)),
-            (text, (x + width / 2 - text.get_width() / 2, y + height / 2 - text.get_height() / 2))
+            (button, pos),
+            (text, (pos[0] + size[0] / 2 - text.get_width() / 2, pos[1] + size[1] / 2 - text.get_height() / 2))
         ])
